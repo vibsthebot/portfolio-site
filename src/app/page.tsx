b653d9387c88d2536eaa39755c19b1a94caa5fb3
vibsthebot <vibhusiddha@gmail.com>
2024-07-11 14:17:39 -0400

    added links and more text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import Image from "next/image";
+import Head from 'next/head'
 
 
 export default function Page() {
@@ -13,9 +15,45 @@ export default function Page() {
           <strong className="text-5xl hover:bg-gradient-to-r from-blue-500 via-green-500 to-sky-500 align-text-bottom hover:text-transparent hover:bg-clip-text">Vibhu Siddha</strong>
         </p>
         <ul className="align-text-bottom">
-          <li>I am a incoming 9th grader</li>
-          <li>I love robotics and reading</li>
-          <li><Link href="https://github.com/vibsthebot" className="text-sky-500 hover:underline">Github</Link></li>
+          <li>i am a incoming 9th grader</li>
+          <li>i love robotics and reading</li>
+          <li>i have a passion for coding</li>
+          <li>i play the french horn</li>
+          <li>i am in scouting</li>
+          <li className="flex items-center pt-2">
+            <div className="flex items-center relative group">
+              <Link href="https://github.com/vibsthebot"  target="_blank">
+                <Image src="/github-mark-white.png"
+                width={25}
+                height={25}
+                alt="Github logo">
+                </Image>
+              </Link>
+              <Link href="https://github.com/vibsthebot"  target="_blank">
+                <p className="opacity-0 text-[0.01px] group-hover:opacity-100 group-hover:pl-2 group-hover:text-base bg-gradient-to-r from-blue-500 via-green-500 to-sky-500 align-text-bottom text-transparent bg-clip-text">vibsthebot </p>
+              </Link>
+            </div>
+            
+            <div className="flex items-center relative group px-2">
+              <Link href="mailto:vibhusiddha@gmail.com">
+                <Image src="/email-logo.png" width={25}
+                  height={25}
+                  alt="Email logo" className="">              
+                </Image>
+              </Link>
+              <Link href="mailto:vibhusiddha@gmail.com">
+                <p className="opacity-0 text-[0.01px] group-hover:opacity-100 group-hover:pl-2 group-hover:text-base bg-gradient-to-r from-blue-500 via-green-500 to-sky-500 align-text-bottom text-transparent bg-clip-text">vibhusiddha@gmail.com </p>
+              </Link>
+            </div>
+            <div className="flex items-center relative group">
+              <Image src="/discord-mark-white.png" width={25}
+                height={25}
+                alt="Discord logo" className="">              
+              </Image>
+              <p className="pacity-0 text-[0.01px] group-hover:opacity-100 group-hover:pl-2 group-hover:text-base bg-gradient-to-r from-blue-500 via-green-500 to-sky-500 align-text-bottom text-transparent bg-clip-text">realvibs </p>
+            </div>
+            
+          </li>
         </ul>
       </div>
       
